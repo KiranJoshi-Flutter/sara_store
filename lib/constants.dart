4a912c13c533c2851a8140046f531875bb1df1a5
kiranjoshi <keronz7777@gmail.com>
2022-03-21 11:56:32 +0545

    Initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color kPrimaryColor = const Color(0xFFea5c25);
+Color kSecondaryColor = const Color(0xFFfea418);
+Color kBackgroundColor = const Color(0xFFfaf6ea);
+// ba6c5c
+Color kDarkBackgroundColor = const Color(0xFFba6c5c);
