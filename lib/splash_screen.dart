72e02be74f740e08546bb78a3bb490b4192c74a0
kiranjoshi <keronz7777@gmail.com>
2022-03-29 16:56:56 +0545

    product_detail_screen completed

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:sara_store/constants.dart';
+import 'package:sara_store/screens/home_screen.dart';
 import 'package:sara_store/screens/main_screen.dart';
 import 'package:show_up_animation/show_up_animation.dart';
 
@@ -24,12 +25,18 @@ class _SplashScreenState extends State<SplashScreen> {
       const Duration(seconds: 3),
       (() {
         Navigator.pop(context);
+        // Navigator.push(
+        //   context,
+        //   MaterialPageRoute(
+        //     builder: (context) => MainScreen(
+        //       title: 'Sara Store',
+        //     ),
+        //   ),
+        // );
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => MainScreen(
-              title: 'Sara Store',
-            ),
+            builder: (context) => HomeScreen(),
           ),
         );
       }),
